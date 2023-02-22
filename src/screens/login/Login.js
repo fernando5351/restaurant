440cd83baca5bba22a7646c5bb09e5a237c8e797
schiffer02 <isaacfernando5351@gmail.com>
2023-02-21 18:50:33 -0600

    agregando login

---
+++ src/screens/login/Login.js
@@ -0,0 +1,16 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React, { useState, useEffect, useContext} from 'react'
+
+export default function Login({navigate}) {
+  return (
+    <View styles={styles.container}>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1
+    }
+})
